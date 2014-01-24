b60dbe92ae213c970ea38b2d7f359211da3536a9
tkmharris <tkmharris@gmail.com>
2014-01-24 22:27:13 +0000

    Improved rolling function.

--- BaileyBot_functions.py
+++ BaileyBot_functions.py
@@ -1,16 +1,19 @@
 import random
+# This file keeps some of the basic functions for BaileyBot.
 
+
+# Rolls a die.
 def dice():
 	return random.randint(1,6)
 
+# Rolls n dice.
 def roll(n):
 	if n < 1:
 	    print "positive integers only please"
-	l = []
-	for i in range(1,n+1):
-	    l = l + [dice()]
-	return l
+	else:
+	    return [dice() for i in range(1, n+1)]
 
+# Counts the number of aces or other numbers.
 def counting(a,b):
 	if b == 1:
 	    return a.count(1)
@@ -24,4 +27,6 @@ r = roll(number)
 print r
 print counting(r,count)
 
-
+# Ideas: Different play styles? Cautious bids the least chance of being caught out. 
+# Aggressive bids as close to 50% as possible.
+# Include bluffs?
