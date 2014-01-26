80d3286ee7f3f6f4caec778b6abf08fa1bcaa2a3
tkmharris <tkmharris@gmail.com>
2014-01-26 20:59:17 +0000

    Entered prob tables. BaileyBot can now look up probabilities taking its own hand into account.

--- BaileyBot_functions.py
+++ BaileyBot_functions.py
@@ -1,5 +1,7 @@
 import random
 import math
+import probabilities
+
 # This file keeps some of the basic functions for BaileyBot.
 
 
@@ -16,15 +18,15 @@ def roll(n):
 
 # Counts the number of aces or other numbers.
 # a is input set, b is number to be counted.
-def counting(hand,number):
+def counting(h,number):
 	if number == 1:
-	    return hand.count(1)
+	    return h.count(1)
 	else:
-	    return hand.count(1) + hand.count(number)
+	    return h.count(1) + h.count(number)
 
 # possible totals.
 def all_totals(m):
-	return [(x,y) for x in range(1, m+1) for y in range(1,7)]
+	return [(x,y) for x in range(1, min(m+1,15)) for y in range(1,7)]
 
 
 # allowable raises
@@ -44,26 +46,45 @@ def allowed(tup1, tup2):
 	    return True
 	else:
 	    return "Something's wrong!"
-	
-#IO stuff
 
-#per = int(raw_input('How many dice?\n'))
-tot = int(raw_input('How many dice in total?\n'))
-#count = int(raw_input('Count which number?\n'))
-i = int(raw_input('bid 1\n'))
-j = int(raw_input('bid 2\n'))
+# Looks up probability on correct table depending on offer (need to add second table).
+def offer_prob(tup, n):
+	if tup[1] != 1:
+	    if tup[0] > 15:
+	        return 0	        
+	    elif tup[0] < 1:
+	        return 100
+	    else:
+		return probabilities.non_ace(tup[0],n)
+	else:	    
+	    if tup[0] > 10:
+	        return 0
+	    elif tup[0] < 1:
+	        return 100	        
+	    else:
+		return probabilities.ace(tup[0],n)
 
-
-
+# IO - currently takes total numeber of dice, number of own dice and offer
+# and calculates probability of offer with own dice taken into account.
+per = int(raw_input('How many dice do you have?\n'))
+tot = int(raw_input('How many dice are there in total?\n'))
+hand = roll(per)
+i = int(raw_input("What's the offer: how many dice?\n"))
+j = int(raw_input("What's the offer: which number on the dice?\n"))
 offer = (i,j)
+offer_mod_own = (i - counting(hand,j), j)
+tot_mod_own = tot - per
+
 all_bids = all_totals(tot)
 poss_bids = [tup for tup in all_bids if allowed(offer,tup)]
 
-print poss_bids
+print hand
+print 'Probability of offer is', offer_prob(offer_mod_own, tot_mod_own),"%."
+
 
 
 # Include method for determining lowest possible raises.
-
-# Ideas: Different play styles? Cautious bids the least chance of being caught out. 
+# Idea: Add lines from Alex's thesis/papers for BaileyBot to say at random.
+# Ideas: Different play styles? Cautious bids the offer with least chance of being caught out. 
 # Aggressive bids as close to 50% as possible.
 # Include bluffs?
